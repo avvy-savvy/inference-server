30dfa03b7d0804c88357ed31c1546b934619e30f
Ravi J <sudha.rkj@gmail.com>
2024-03-09 21:32:57 -0800

    temp commit

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,13 @@
 ARG BASE_IMAGE=tensorflow/tensorflow:2.13.0-gpu
 FROM $BASE_IMAGE
 
+#RUN apt-get update && apt-get install -y zsh && apt-get install -y git
+
+#RUN chsh -s $(which zsh)
+
+# Install oh-my-zsh
+#RUN sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
 # Upgrade pip
 RUN python3 -m pip install --upgrade pip
 
@@ -12,6 +19,7 @@ RUN python3 -m pip install -r /tmp/requirements.txt
 
 # Copy source code
 COPY app.py /app/app.py
+COPY data /app/data
 
 # Set the working directory
 WORKDIR /app
@@ -19,9 +27,14 @@ WORKDIR /app
 # Set flask environment labels
 ENV FLASK_APP=app
 ENV FLASK_ENV=development
+ARG HOST_URI=0.0.0.0
+ARG HOST_PORT=5000
+# Entrypoint are unable to use arg values and so converting to environment variables
+ENV APP_URI=$HOST_URI
+ENV APP_PORT=$HOST_PORT
 
 # Expose port
-EXPOSE 5000
+EXPOSE ${HOST_PORT}
 
 # Set the entrypoint
-ENTRYPOINT [ "flask", "run", "--host=0.0.0.0" ]
+ENTRYPOINT flask run --host="${APP_URI}" -p "${APP_PORT}"
--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask, request
+import os
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 @app.route('/danger_scale', methods=['GET'])
@@ -11,6 +19,7 @@ def get_danger_scale():
 
 @app.route('/treeline_labels', methods=['GET'])
 def get_treeline_labels():
+    print(f"App URI: {os.environ.get('HOST_URI')}")
     # TODO add validations on the args and perform inference
     latitude = request.args.get('lat')
     longitude = request.args.get('long')
@@ -24,3 +33,65 @@ def get_treeline_labels():
         'Near Treeline': '2',
         'Below Treeline': '1'
     }
+
+
+@app.route('/query_arrival_info', methods=['POST'])
+def query_arrival_info():
+    # global additional_feature
+    # global model
+
+    # Load additional features
+    additional_feature = pd.read_csv("data/additional_feature.csv", keep_default_na=False)
+    # Load model
+    model = tf.keras.models.load_model("data/tf_rand_val_nn_v1")
+
+    request_data = request.get_json()
+
+    print(f"App URI: {os.environ.get('HOST_URI')}")
+    # TODO add validations on the args and perform inference
+    print(f'Requested arriavl info for\n'
+          f'  aspect: {request_data["aspect"]}, \n'
+          f'  elevation: {request_data["elevation"]}, \n'
+          f'  date: {request_data["date"]}')
+
+    input_features = pd.DataFrame(
+        {'date_time': [request_data['date']], 'combined_terrain_aspects': [request_data['aspect']],
+         'combined_terrain_elevations': [request_data['elevation']]})
+    all_features = pd.merge(input_features, additional_feature, on='date_time', how='inner')
+
+    input_dataset = df_to_dataset(all_features)
+    predicted = model.predict(input_dataset)
+    predictions = np.transpose(predicted)[0]
+
+    return jsonify({
+        'prob': str(predictions[0])
+    })
+
+
+def df_to_dataset(dataframe, shuffle=True, batch_size=32):
+    df = dataframe.copy()
+    labels = None  # Initialize labels as None
+
+    # Remove the target column if it exists
+    if 'target' in df.columns:
+        labels = df.pop('target')  # Remove and store the target column
+
+    # Convert the dataframe to a dictionary of lists
+    df_dict = {key: value.tolist() for key, value in df.items()}
+
+    # Create a dataset from the dictionary and labels
+    ds = tf.data.Dataset.from_tensor_slices((df_dict, labels))
+
+    # Shuffle the dataset if specified
+    if shuffle:
+        ds = ds.shuffle(buffer_size=len(dataframe))
+
+    # Batch and prefetch the dataset
+    ds = ds.batch(batch_size)
+    ds = ds.prefetch(batch_size)
+
+    return ds
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('HOST_URI', "0.0.0.0"), port=os.environ.get('HOST_PORT', 5000))
